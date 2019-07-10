5f91d0e746bcea2d552df4733ff05289e0d3c239
popejb1116 <popejb1116@gmail.com>
2019-07-10 13:46:18 -0400

    Basic file structure built

---
+++ src/layout/GridLayout.svelte
@@ -0,0 +1,12 @@
+<script>
+   import Header from './header/Header.svelte'
+   import Content from './content/Content.svelte'
+   import Aside from './aside/Aside.svelte'
+   import Footer from './footer/Footer.svelte'
+</script>
+
+
+<Header/>
+<Content/>
+<Aside/>
+<Footer/>
