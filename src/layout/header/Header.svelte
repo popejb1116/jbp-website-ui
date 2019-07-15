c1a3a48e71ef596c30f7442618fb12069c3a6e4c
popejb1116 <popejb1116@gmail.com>
2019-07-15 19:01:45 -0400

    Hide mobile nav toggle button in Header at 768px and added Logo component to Header

--- src/layout/header/Header.svelte
+++ src/layout/header/Header.svelte
@@ -1,15 +1,23 @@
 <script>
-   import { getContext } from 'svelte'
+   import {displayMobileNav} from '../../stores/MobileNavDisplay'
+   import Logo from './Logo.svelte'
+   import MobileNavToggle from '../../stores/MobileNavToggle.svelte'
 </script>
 
 <style>
    .header {
       grid-area: head;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
       background: var(--theme-primary);
       color: var(--theme-secondary);
    }
 </style>
 
 <div class="header">
-   Head
+   <Logo/>
+   <MobileNavToggle/>
 </div>
---
+++ src/stores/MobileNavToggle.svelte
@@ -0,0 +1,27 @@
+<script>
+   import {displayMobileNav} from './MobileNavDisplay'
+
+   function toggle() {
+      displayMobileNav.update(state => !state)
+   }
+</script>
+
+<style>
+   #toggle {
+      margin: 0 1rem 0 0;
+      position: absolute;
+      right: 0;
+   }
+   @media (min-width: 768px) {
+      #toggle {
+         display: none;
+      }
+   }
+</style>
+
+<button 
+   id="toggle"
+   on:click={toggle}
+>
+   Toggle Mobile Nav
+</button>
