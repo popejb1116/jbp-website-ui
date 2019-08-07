f0254739e023c0c599917affbfb795319c146dab
popejb1116 <popejb1116@gmail.com>
2019-08-06 21:54:15 -0400

    Edit styling of YouTubeCard

--- src/layout/pages/projects/YouTubeCard.svelte
+++ src/layout/pages/projects/YouTubeCard.svelte
@@ -1,23 +1,45 @@
 <style>
    .youtube-card {
-      width: 40vw;
+      display: flex;
+      flex-flow: column nowrap;
+      justify-content: flex-start;
+      align-items: center;
+
+      width: 100%;
       height: auto;
       
       margin: 0.75rem 0;
 
-      background: var(--theme-accent);
-      box-shadow: 2px 2px 4px var(--theme-accent);
+      background: transparent;
+      box-shadow: 2px 2px 4px var(--theme-primary);
+      border-radius: 2px;
+   }
+   .iframe-container {
+      display: flex;
+      justify-content: center;
+
+      width: 100%;
+      padding: 0.25rem 0;
+      
+      background: rgba(0, 0, 0, 0.5);
+      border-top-left-radius: 2px; 
+      border-top-right-radius: 2px;
    }
    iframe {
       width: 40vw;
       height: 24vw;
+      min-width: 320px;
+      min-height: 192px;
+      border-radius: 5px;
    }
    .body {
-      height: 100%;
       padding: 1rem;
+      background: var(--theme-primary);
+      border-bottom-left-radius: 2px; 
+      border-bottom-right-radius: 2px; 
    }
    .title {
-      color: var(--theme-primary);
+      color: var(--theme-accent);
       margin-bottom: 1rem;
       text-align: center;
       letter-spacing: 0.3rem;
@@ -33,11 +55,13 @@
 </style>
 
 <div class="youtube-card">
-   <iframe 
-      src="https://www.youtube.com/embed/35mFPbNE1iU" 
-      frameborder="0" 
-      title="Responsive Navbar">
-   </iframe>
+   <div class="iframe-container">
+      <iframe 
+         src="https://www.youtube.com/embed/35mFPbNE1iU" 
+         frameborder="0" 
+         title="Responsive Navbar">
+      </iframe>
+   </div>
 
    <div class="body">
       <div class="title">Foyer into YouTube oblivion...</div>
