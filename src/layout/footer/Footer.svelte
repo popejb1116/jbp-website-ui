84ba4c86a8629cbb7cd15970e9f478b4045d676b
popejb1116 <popejb1116@gmail.com>
2019-08-08 14:51:17 -0400

    Official statement added to Footer

--- src/layout/footer/Footer.svelte
+++ src/layout/footer/Footer.svelte
@@ -4,9 +4,28 @@
       background: var(--theme-accent);
       border-top-left-radius: 2px;
       border-top-right-radius: 2px;
+
+      padding: 1rem;
+   }
+   .statement {
+      color: white;
+      font: 300 1rem 'Ubuntu';
+   }
+   .sm-icons {
+      display: none;
+   }
+   @media(max-width: 768px) {
+      .statement {
+         font-size: 0.5rem;
+      }
+      .sm-icons {display: inline;}
    }
 </style>
 
 <div class="footer">
-   Footer
+   <span class="statement">
+      This webpage was soley developed and is independently maintained by Jonathan Pope.
+      Please see the contact page for any related inquiries.
+   </span>
+   <span class="sm-icons">SVG SVG</span>
 </div>
