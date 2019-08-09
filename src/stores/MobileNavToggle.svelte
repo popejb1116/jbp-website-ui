47a6f669bef248c4625c694f607ea8c968b411cf
popejb1116 <popejb1116@gmail.com>
2019-08-09 15:05:22 -0400

    Changed mobile open icon, now centers properly

--- src/stores/MobileNavToggle.svelte
+++ src/stores/MobileNavToggle.svelte
@@ -1,6 +1,5 @@
 <script>
    import {toggleMobileNav} from './MobileNavDisplay'
-   // let navIcon = '/images/icons/icon_mobile-nav.png'
    let navIcon = '/images/icons/icon_open-mobile.svg'
 </script>
 
@@ -15,11 +14,6 @@
       background: transparent;
 
       border: solid 1px red;
-   }
-   img {
-      width: 100%;
-      height: 100%;
-      padding-left: 25%;
    }
    /* GRIDLAYOUT HIDES ASIDE AT 768 */
    @media (min-width: 769px) {
